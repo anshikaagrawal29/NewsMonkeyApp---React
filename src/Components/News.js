940f162b8faa7e1cfa8dce1fc7873f8f9161df07
Anshika Agrawal <workwithanshika@gmail.com>
2022-07-28 17:12:38 +0530

    Added buttons and added if else logic

--- src/Components/News.js
+++ src/Components/News.js
@@ -9,20 +9,52 @@ export default class News extends Component {
   {
     super();
     this.state = {
-      articles : []
+      articles : [],
+      page: 1,
+      totalResults : 0
     };
   }
 
   // aysnc will wait till the await function returns promise 
   async componentDidMount()
   {
-      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=8c0aec2ab08f43cc8ca385c08fb96cde";
+      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=8c0aec2ab08f43cc8ca385c08fb96cde&page=1&pageSize=5";
       //fetch is used to get the data from api
       let data = await fetch(url);
       let parsedData = await data.json();
       //This is how we set the state variable
-      this.setState({articles : parsedData.articles})
+      this.setState({articles : parsedData.articles, totalResults : parsedData.totalResults})
   }
+
+  HandlePreviousBtnClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8c0aec2ab08f43cc8ca385c08fb96cde&page=${this.state.page - 1}&pageSize=5`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page : this.page - 1,
+      articles : parsedData.articles
+    })
+    
+  };
+
+  HandleNextBtnClick = async () => {
+
+    if(this.state.page + 1 >  Math.ceil(this.state.totalResults/5))
+    {
+    
+    }
+    else
+    {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8c0aec2ab08f43cc8ca385c08fb96cde&page=${this.state.page +1 }&pageSize=5`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page : this.page + 1,
+        articles : parsedData.articles
+      })
+    }
+    
+  };
 
   render() {
     return (
@@ -36,6 +68,10 @@ export default class News extends Component {
             imageUrl = {element.urlToImage? element.urlToImage: "https://images.moneycontrol.com/static-mcnews/2022/07/sensex_nifty-marketup_sensexup-Niftyup-4-770x433.jpg"} />
             </div>
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button type="button" disabled= {this.state.page<=1} className="btn btn-dark" onClick={this.HandlePreviousBtnClick}> &larr; Previous</button>
+          <button type="button"  className="btn btn-dark" onClick={this.HandleNextBtnClick}>Next &rarr; </button>
         </div>
       </div>
     )
